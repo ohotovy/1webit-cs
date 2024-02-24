b8127a8ba17bbef7ab7b94c86b4da10aa901a361
Ondřej Hotový <ondrej.hotovy@outlook.com>
2024-02-24 11:49:07 +0100

    initial state

---
+++ app/Presenters/HomePresenter.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Presenters;
+
+use Nette;
+
+
+final class HomePresenter extends Nette\Application\UI\Presenter
+{
+	public function __construct(
+		private Nette\Database\Explorer $database,
+	) {
+	}
+
+    public function renderDefault(): void
+    {
+        $this->template->posts = $this->database
+            ->table('posts')
+            ->limit(5);
+    }
+
+	// ...
+}
